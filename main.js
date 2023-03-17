055fd7e5aa78acc4caf52a5dd8b18f6c71dc9192
Hodan Suleiman <suleimanhodan9@gmail.com>
2023-03-17 16:03:23 -0400

    artQuiz V2 attributes and multiple pages with sets

--- main.js
+++ main.js
@@ -1,1 +1,76 @@
-// JavaSript code here
+ const apiUrl = "https://api.artic.edu/api/v1/artworks?limit=60";
+    const view = document.getElementById("output");
+    const refreshButton = document.getElementById("refresh");
+    const rounds = document.getElementById("round-count");
+
+    let count = 0;
+    let imgUrlArray = [];
+    let artistName = [];
+    let artTitle = [];
+
+    // fetching the images from the API
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        data.data.forEach(imgObject => {
+          const imageId = imgObject.image_id;
+          const artist = imgObject.artist_titles;
+          const title = imgObject.title;
+          const imageUrl = `https://www.artic.edu/iiif/2/${imageId}/full/843,/0/default.jpg`;
+
+          imgUrlArray.push(imageUrl);
+          artistName.push(artist);
+          artTitle.push(title);
+        });
+      });
+
+    // creating images for the Art API
+    const artImg = () => {
+      // check if we've reached the maximum number of rounds
+      if (count >= 10) {
+        refreshButton.innerText = "Game Over";
+        return;
+      }
+
+      // update the round count in the UI
+      rounds.innerText = `Round ${count + 1}`;
+
+      // clear the output container and shuffle the image URLs
+      view.innerHTML = "";
+      shufflePicture();
+
+      // create and display the images
+      for (let i = 0; i < 4; i++) {
+        const div = document.createElement("div");
+        div.className = "art-img";
+        const artPiece = document.createElement("img");
+        let url = imgUrlArray[i];
+        artPiece.src = url;
+        artPiece.setAttribute("data-artist", artistName[i]);
+        artPiece.setAttribute("data-title", artTitle[i]);
+        div.append(artPiece);
+        view.append(div);
+
+        // add event listener to log artist name and title to console
+        artPiece.addEventListener("click", () => {
+          console.log(`Artist: ${artPiece.dataset.artist} Title: ${artPiece.dataset.title}`);
+        });
+      }
+
+      // increment the round count
+      count++;
+    };
+
+    // shuffling images for the refresh button
+    const shufflePicture = () => {
+      for (let i = 0; i < imgUrlArray.length; i++) {
+        let random = Math.floor(Math.random() * imgUrlArray.length);
+        let temp = imgUrlArray[i];
+        imgUrlArray[i] = imgUrlArray[random];
+        imgUrlArray[random] = temp;
+      }
+    };
+
+    refreshButton.addEventListener("click", artImg);
+
+    
